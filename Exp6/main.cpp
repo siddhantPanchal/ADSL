8bda7920220e08e4bdf65837b7277ebf5e4ae701
siddhantPanchal <55625388+siddhantPanchal@users.noreply.github.com>
2020-02-17 23:22:27 +0530

    Create main.cpp

---
+++ Exp6/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include "Dictionary.h"
+using namespace std;
+
+int main()
+{
+	Dictionary d;
+	int ch = 0;
+	do
+	{
+		cout << "\n1.insert new value \n2.display \n3.delete \n10.exit \nEnter your choice : ";
+		cin >> ch;
+		switch (ch)
+		{
+		case 1:d.insert();
+			break;
+		case 2:d.display();
+			break;
+		case 3:d.delet();
+			break;
+		case 10:
+			exit(0);
+		default:
+			break;
+		}
+	} while (true);
+	return 0;
+}
